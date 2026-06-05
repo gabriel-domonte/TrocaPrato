fc0f8da20a073e266e1e0e2bd5387b110c0becd3
gabriel-domonte <gabrieldomontelima@gmail.com>
2026-06-05 14:44:53 -0300

    refactor: remove bloco de código com lógica de negócio, deixando apenas rotas e blueprints

--- app.py
+++ app.py
@@ -1,61 +1,20 @@
-from flask import Flask, render_template, request, send_file
-import csv
+from flask import Flask, render_template, send_file
+from app.models.carregar_alimentos import alimentos
+from app.controller.comparador import comparador_bp
 
-app = Flask(__name__)
+app = Flask(__name__,
+            template_folder='app/templates',
+            static_folder='app/static')
 
-@app.route('/data/taco_adaptada.csv')
+@app.route('/app/data/taco_adaptada.csv')
 def csv_file():
-    return send_file('data/taco_adaptada.csv')
+    return send_file('app/data/taco_adaptada.csv')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/resultado', methods=['POST'])
-def resultado():
-
-    cal_porcao = 0
-
-    dic_alimento_a = {}
-    dic_alimento_b = {}
-
-    id_a = request.form['alimento-a']
-    id_b = request.form['alimento-b']
-    peso_a = int(request.form['peso_a'])
-
-    with open('data/taco_adaptada.csv', newline='', encoding='utf-8') as tabela:
-        leitor = csv.DictReader(tabela)
-        for linha in leitor:
-            if linha['id_alimento'] == id_a:
-                cal_porcao = peso_a * float(linha['kcal']) / 100
-                dic_alimento_a = {
-                    'nome': f'{linha['descricao']} ({linha['tags']})' if linha['tags'] else f'{linha['descricao']}',
-                    'peso': round(peso_a),
-                    'cal': round(cal_porcao),
-                    'carb': round(peso_a * float(linha['carboidratos']) / 100),
-                    'prot': round(peso_a * float(linha['proteinas']) / 100),
-                    'lip': round(peso_a * float(linha['lipidios']) / 100),
-                    'fib': round(peso_a * float(linha['fibras']) / 100)
-                }
-                break
-
-    with open('data/taco_adaptada.csv', newline='', encoding='utf-8') as tabela:
-        leitor = csv.DictReader(tabela)
-        for linha in leitor:
-            if linha['id_alimento'] == id_b:
-                peso_b = cal_porcao * 100 /float(linha['kcal'])
-                dic_alimento_b = {
-                    'nome': f'{linha['descricao']} ({linha['tags']})' if linha['tags'] else f'{linha['descricao']}',
-                    'peso': round(peso_b),
-                    'cal': round(cal_porcao),
-                    'carb': round(peso_b * float(linha['carboidratos']) / 100),
-                    'prot': round(peso_b * float(linha['proteinas']) / 100),
-                    'lip': round(peso_b * float(linha['lipidios']) / 100),
-                    'fib': round(peso_b * float(linha['fibras']) / 100)
-                }
-                break
-
-    return render_template('resultado.html', alimento_a=dic_alimento_a, alimento_b=dic_alimento_b)
+app.register_blueprint(comparador_bp)
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
