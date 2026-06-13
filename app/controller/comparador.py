9f9053ae1377e647d736957f286ca9558f87ecf9
gabriel-domonte <gabrieldomontelima@gmail.com>
2026-06-13 13:12:34 -0300

    fix: redireciona o usuário caso ele não esteja autenticado

--- app/controller/comparador.py
+++ app/controller/comparador.py
@@ -1,11 +1,17 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, session, redirect
 from app.models.carregar_alimentos import alimentos
 from app.models.comparador import alimento_a, alimento_b
 
 comparador_bp = Blueprint('comparador', __name__)
 
-@comparador_bp.route('/resultado', methods=['POST'])
+@comparador_bp.route('/resultado', methods=['POST', 'GET'])
 def comparador():
+    #caso o usuário não esteja logado ou acesse a página /resultado através de uma requisição GET, redireciona o usuário para a página correta
+    if 'usuario_logado' not in session:
+        return redirect('/')
+    elif request.method == 'GET':
+        return redirect('/comparador')
+    
     #recebe dados do formulário, chama as funções do comparador e retorna uma comparação na forma de de dois dicionários
     id_a = int(request.form['alimento-a'])
     id_b = int(request.form['alimento-b'])
@@ -15,4 +21,3 @@ def comparador():
     dict_alimento_b = alimento_b(alimentos, id_b, dict_alimento_a)
 
     return render_template('resultado.html', dict_alimento_a=dict_alimento_a, dict_alimento_b=dict_alimento_b)
-
