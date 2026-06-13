b8df3bb361652fc6532ffcfaaeb846411bdb4c8c
gabriel-domonte <gabrieldomontelima@gmail.com>
2026-06-13 13:19:57 -0300

    feat: implementa novas rotas e blueprints para autenticação e página inicial

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
-from flask import Flask, render_template, send_file
-from app.models.carregar_alimentos import alimentos
+from flask import Flask, render_template, send_file, session, redirect
+from config import Config
 from app.controller.comparador import comparador_bp
+from app.controller.autenticacao import autenticacao_bp
 
 app = Flask(__name__,
             template_folder='app/templates',
             static_folder='app/static')
 
+app.config.from_object(Config)
+
 @app.route('/app/data/taco_adaptada.csv')
-def csv_file():
+def taco_adaptada():
     return send_file('app/data/taco_adaptada.csv')
 
 @app.route('/')
 def index():
+    if 'usuario_logado' in session:
+        return redirect('/comparador')
+    
     return render_template('index.html')
 
-app.register_blueprint(comparador_bp)
+@app.route('/comparador')
+def comparador():
+    if 'usuario_logado' not in session:
+        return redirect('/')
+    
+    return render_template('comparador.html', erros={})
+
+app.register_blueprint(comparador_bp, url_prefix='/comparador')
+
+app.register_blueprint(autenticacao_bp)
     
 if __name__ == '__main__':
     app.run(debug=False)
