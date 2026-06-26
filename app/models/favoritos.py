f4f7505687850d926f61b5d9084deb753c186579
danielemoraissilva137-coder <danielemoraissilva137@gmail.com>
2026-06-26 19:44:19 -0300

    Ajusta funções defavoritos e do html

--- app/models/favoritos.py
+++ app/models/favoritos.py
@@ -4,22 +4,18 @@ from app.models.comparador import alimento_a, alimento_b
 ARQUIVO = 'app/data/favoritos.csv'
 
 def verificar_favorito(user_id, id_alimento_a, id_alimento_b, peso_a):
-    try:
-        with open(ARQUIVO, 'r') as favoritos:
-            linhas = favoritos.readlines()
-        if len(linhas) <= 1:
-            return False
-        for linha in linhas[1:]:
-            linha = linha.strip().split(",")
-            if len(linha) < 4: 
-                 continue
-
-            if  linha[0] == user_id and linha[1] == id_alimento_a and linha[2] == id_alimento_b and linha[3] == peso_a:
-                return True
+    with open(ARQUIVO, 'r') as favoritos:
+        linhas = favoritos.readlines()
+    if len(linhas) <= 1:
         return False
-    except FileNotFoundError:
-        return False
+    for linha in linhas[1:]:
+        linha = linha.strip().split(",")
+        if len(linha) < 4: 
+            continue
 
+        if  linha[0] == user_id and linha[1] == id_alimento_a and linha[2] == id_alimento_b and linha[3] == peso_a:
+            return True
+    return False
 
 def salvar_favorito(user_id, id_alimento_a, id_alimento_b, peso_a):
     if verificar_favorito(user_id, id_alimento_a, id_alimento_b, peso_a):
@@ -32,24 +28,20 @@ def salvar_favorito(user_id, id_alimento_a, id_alimento_b, peso_a):
 
 def listar_favoritos(user_id):
     lista_favoritos = []
+    with open(ARQUIVO, 'r') as favoritos:
+        next(favoritos)
+        for linha in favoritos:
+            linha = linha.strip().split(",")
+            if len(linha) < 4:
+                continue
+            if user_id == linha[0]:
+                id_alimento_a = linha[1]
+                id_alimento_b = linha[2]
+                peso_a = linha[3]
 
-    try:
-        with open(ARQUIVO, 'r') as favoritos:
-            next(favoritos)
-            for linha in favoritos:
-                linha = linha.strip().split(",")
-                if len(linha) < 4:
-                    continue
-                if user_id == linha[0]:
-                    id_alimento_a = linha[1]
-                    id_alimento_b = linha[2]
-                    peso_a = linha[3]
-
-                    if not id_alimento_a or not id_alimento_b or not peso_a:
-                        continue 
-                    lista_favoritos.append((int(id_alimento_a), int(id_alimento_b), int(peso_a)))
-    except FileNotFoundError:
-        return []
+                if not id_alimento_a or not id_alimento_b or not peso_a:
+                    continue 
+                lista_favoritos.append((int(id_alimento_a), int(id_alimento_b), int(peso_a)))
     return lista_favoritos
         
 
@@ -64,7 +56,6 @@ def favoritos_detalhados(user_id):
         resultado_b = alimento_b(alimentos, id_alimento_b, resultado_a)
         resultado.append({
             'id_alimento_a': id_alimento_a,
-            'id_alimento_b': id_alimento_b,
             'nome': resultado_a['nome'],
             'tags': resultado_a['tags'],
             'peso_a': resultado_a['peso'],
@@ -73,8 +64,14 @@ def favoritos_detalhados(user_id):
             'carboidratos': resultado_a['carb'],
             'lipideos': resultado_a['lip'],
 
+            'id_alimento_b': id_alimento_b,
             'nome_b': resultado_b['nome'],
-            'peso_b': resultado_b['peso']
+            'tags_b': resultado_b['tags'],
+            'peso_b': resultado_b['peso'],
+            'calorias_b': resultado_b['cal'],
+            'proteinas_b': resultado_b['prot'],
+            'carboidratos_b': resultado_b['carb'],
+            'lipideos_b': resultado_b['lip'],
         })
     return resultado
 
@@ -97,5 +94,5 @@ def remover(user_id, id_alimento_a, id_alimento_b, peso_a):
             favoritos_atualizado.append(",".join(linha) + "\n")
     with open(ARQUIVO, 'w') as favoritos:
         favoritos.writelines(favoritos_atualizado)
-            
     return favorito_removido
+ 
