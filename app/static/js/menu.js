6e6511e0d5ffc01f19d5cca7fab7013cc67a980a
Laura Epaminondas <analauraepaminondas2018@gmail.com>
2026-07-01 11:30:30 -0300

    fix: ajustes mínimos no css

--- app/static/js/menu.js
+++ app/static/js/menu.js
@@ -1,9 +1,12 @@
+// guardando os elementos do html
 const menu = document.getElementById('nav-menu');
 const button = document.querySelector('.botao-burguer');
 
 
-/* toggle - alterna classe a cada clique*/
+// toggle - alterna classe a cada clique
 button.addEventListener('click', () => {
+    // exibição do navegador
     menu.classList.toggle('aberto');
+    // mudança de animação do botão
     button.classList.toggle('ativo');
 });
